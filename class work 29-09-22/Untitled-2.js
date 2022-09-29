1e875d1091e30dc2b14f6510f131bec74a6866a4
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-09-29 11:55:50 +0100

    Add files via upload

---
+++ class work 29-09-22/Untitled-2.js
@@ -0,0 +1,32 @@
+//number1
+console.log("* is an operator");
+console.log("hello is a value");
+console.log("-88.8 is a value");
+console.log("- is an operator");
+console.log("/ is an operator");
+console.log("+ is an operator");
+console.log("5 is a value");
+//number2
+console.log("spam is not a string");
+console.log("'spam' is not a string");
+//number3
+console.log("data types include, Numbers, Strings and Booleans");
+//number4
+console.log("bacon = 21");
+//number5
+console.log("spamspamspam");
+console.log("NaN");
+//number6
+let UserImput = Number(prompt("enter what number of greeting you want"));
+if(UserImput=1){
+    console.log("Hello");
+}else if(UserImput=2){
+    console.log("Howdy");
+}else{
+    console.log("Greetings");
+}
+//number7
+console.log("Top-Left error = capital C in console.log");
+console.log("Top-Right error = all letters in capitals");
+console.log("Bottom-Left error = capital L in Log");
+console.log("Bottom-Right error = . has been replaced by space between console and log");
