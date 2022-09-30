8a815f0283f0a63039181445865f730f2309d1cc
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-09-30 11:45:20 +0100

    assignment3

--- class work 29-09-22/Untitled-2.js
+++ class work 29-09-22/Untitled-2.js
@@ -30,3 +30,12 @@ console.log("Top-Left error = capital C in console.log");
 console.log("Top-Right error = all letters in capitals");
 console.log("Bottom-Left error = capital L in Log");
 console.log("Bottom-Right error = . has been replaced by space between console and log");
+//number8
+console.log("This will not print anything");
+console.log("This will print the value for the varianle 'Hello World!'");
+console.log("This will not print anything");
+console.log("This will print 'Hello World1'");
+console.log("This will hide 'console.log(Hello World!)");
+console.log("This will return as an error");
+console.log("This will print 'Hello World!' and then 'Welcome to JavaScript' underneath it");
+console.log("This is will hide all the elements of code as a comment");
