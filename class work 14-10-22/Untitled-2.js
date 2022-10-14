6bf631186f160e83f9f2b9bebced8950c6cb4306
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-10-14 12:35:31 +0100

    while loop work

---
+++ class work 14-10-22/Untitled-2.js
@@ -0,0 +1,47 @@
+//not tested as html file would not open
+//question 1
+let number1 = 1
+while(number1<=10){
+    if(number1==1){
+        console.log(number1);
+        number1++;
+    }
+}
+//question 2
+let number2 = 1
+do{
+    number2++;
+    console.log(number2);
+}while(number2<=100){
+    number2+1;
+    console.log(number2);
+}
+//number 3
+let number3 = Number(prompt("enter the root number"));
+let number4 = Number(prompt("enter the power"));
+let = number5 = number3
+while(number3!=number3**number4){
+    number3*number5
+}
+console.log(number3);
+//number 4
+let number6 = Number(prompt("enter the number of rows"));
+let number7 = "*"
+do{
+    console.log(number7);
+    number7=number7+"*";
+}while(number6<=number7);
+//number 5  
+let number8 = Number(prompt("enter your number"));
+let number9 = 0
+while(number8<number9){
+    number8=math.floor(number8/10);
+    number9++;
+}
+console.log(number8 +"is the number of digits in"+number9);
+//number 6
+let number10 = Number(prompt("enter your number"));
+while(number10>0){
+    let number11 = 
+    number10=math.floor(number10/10);
+}
