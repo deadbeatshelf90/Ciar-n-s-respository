3451c0454ee07577b4accd4fe22ffb54650f8f2d
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-10-24 11:56:40 +0100

    loop exercise

--- class work 21-10-22/Untitled-1.js
+++ class work 21-10-22/Untitled-1.js
@@ -23,31 +23,41 @@ if(a==1){
     let h =Number(prompt("enter your number"));
     let i =0
     let g=h
-    for(g>0;;){
+    for(;g>0;){
         i=g%10
         g=Math.floor(g/10);
-        if(i=0){
+        if(i==0){
             console.log("zero");
-        }else if(i=1){
+        }else if(i==1){
             console.log("one");
-        }else if(i=2){
+        }else if(i==2){
             console.log("two");
-        }else if(i=3){
+        }else if(i==3){
             console.log("three");
-        }else if(i=4){
+        }else if(i==4){
             console.log("four");
-        }else if(i=5){
+        }else if(i==5){
             console.log("five");   
-        }else if(i=6){
+        }else if(i==6){
             console.log("six");
-        }else if(i=7){
+        }else if(i==7){
             console.log("seven");
-        }else if(i=8){
+        }else if(i==8){
             console.log("eight");
         }else{
             console.log("nine");
-        }
+        }   
     }   
+}else if(a==4){
+    //number4
+    let j = Number(prompt("enter number 1"));
+    let k = Number(prompt("enter number 2"));
+    let max = Math.max(j,k);
+    for(;(max/j==Math.floor(max/j)&&max/k==Math.floor(max/k))==false;){
+        max=max*2
+    }
+    console.log(max+" is the lcm of "+j+" and "+k);
+
 }else{
 console.log("thats not a question");
 }
