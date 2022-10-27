359d99ea6586a0a3a374ea2cb32ebf6d5d183a8e
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-10-27 11:56:51 +0100

    Add files via upload

--- class work 26-10-22/Untitled-1.js
+++ class work 26-10-22/Untitled-1.js
@@ -22,7 +22,26 @@ if(a==1){
     }
 }else if(a==2){
     //number2
-
+    let h= Number(prompt("enter your number"));
+    let i=h
+    let j=i
+    let k=1
+    let l=0
+    for(;i>0;){
+        j=j%10
+        for(;j>0;){
+            k=k*j
+            j--
+        } 
+        l=l+k
+        k=1
+        i=Math.floor(i/10);
+    }
+    if(l==h){
+        console.log(h+" is a strong number");
+    }else{
+        console.log(h+" is not a strong number");
+    }
 }else{
     console.log("that is not a question");
 }
