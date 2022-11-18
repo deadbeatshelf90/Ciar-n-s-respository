88b9d6bd12545f9ec65ceeaeb69813f50181078d
deadbeatshelf90 <112865093+deadbeatshelf90@users.noreply.github.com>
2022-11-18 12:12:56 +0000

    Add files via upload

--- class work 16-11-22/array.js
+++ class work 16-11-22/array.js
@@ -1,7 +1,7 @@
 let myFace = new Array("tea","coffee",-1,2);
 let a = Number(prompt("what question do you want?"));
 if(a==1){
-    document.write(typeof(myFace[2]));
+    document.write(myFace[2]);
 }else if(a==2){
     for(let b=0;b<myFace.length+1;b++){
         if(typeof(myFace[b])=="number"){
@@ -39,20 +39,61 @@ if(a==1){
     let f = 0
     for(let b=0;b<myFace.length+1;b++){
         if(typeof(myFace[b])=="number"){
-          if(myFace[b]>f){
-            f=myFace[b]
-          }  
+            if(myFace[b]>f){
+                f=myFace[b]
+            if(f==0){
+                f=myFace[b];
+            }
+            }  
         }
     }
     let g = 0
-    for(let b=0;b<myFace.length+1;b++){
-        if(typeof(myFace[b])=="number"||myFace[b]!=f){
-            if(myFace[b]>g){
-                g = myFace[b]
+    for(let h=0;h<myFace.length+1;h++){
+        if(typeof(myFace[h])=="number"){
+            if(myFace[h]<f){
+                if(myFace[h]>g){
+                    g = myFace[h];
+                }
+                if(g==0){
+                    g=myFace[h];
+                }
             }
         }
     }
     document.write(g);
+}else if(a==6){
+    let j=0
+    let k=0
+    for(let i=0;i<myFace.length+1;i++){
+        if(typeof(myFace[i])=="number"){
+            if(myFace[i]%2==0){
+                j++;
+            }else{
+                k++
+            }
+        }
+    }
+    document.write(j+" is the number of even numbers");
+    document.write("<br>");
+    document.write(k+" is the number of odd numbers");
+}else if(a==7){
+    let myArray= new Array();
+    for(let l=0;l<myFace.length+1;l++){
+        myArray.push(myFace[l]);
+    }
+    document.write("<br>");
+    document.write(myArray);
+}else if(a==8){
+    let m = prompt("which do you wish to add? String or Number");
+    if(m=="String"){
+        let m = prompt("enter the string you wish to add");
+    }
+    if(m=="Number"){
+        let m = Number(prompt("enter the number you wish to add"));
+    }
+    myArray.push(m);
+}else if(a==9){
+    myArray.pop();
 }else{
     console.log("that is not a question");
 } 
